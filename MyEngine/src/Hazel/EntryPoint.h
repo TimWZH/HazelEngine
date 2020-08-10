89f83854d14d7df0acdcaf4e1cfcd0c30895d886
unknown <120150314@qq.com>
2020-08-10 15:08:16 -0400

    Logging

--- MyEngine/src/Hazel/EntryPoint.h
+++ MyEngine/src/Hazel/EntryPoint.h
@@ -5,6 +5,12 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello var={0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 
---
+++ MyEngine/src/Hazel/Log.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Hazel{
+	class HZ_API  Log
+	{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger;}
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define HZ_CORE_ERROR(...)    ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_WARN(...)     ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_INFO(...)     ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_TRACE(...)    ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_CRITICAL(...)    ::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define HZ_ERROR(...)	    ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_WARN(...)		::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_INFO(...)		::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_TRACE(...)	    ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_CRITICAL(...)	    ::Hazel::Log::GetClientLogger()->critical(__VA_ARGS__)
